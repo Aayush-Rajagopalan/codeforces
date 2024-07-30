787ac690f629cec4b0f700b4edcb44ffc43c6f16
Aayush Rajagopalan <61064137+Aayush-Rajagopalan@users.noreply.github.com>
2024-07-30 13:49:44 +0300

    chore: Update +page component with codeforces instructions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex max-w-4xl flex-col gap-4 mt-4">
+	<h1>codeforces</h1>
+	<p>find the problem ID and enter it to the site to view the problem</p>
+    <p>for example: 4A. Watermelon, open https://cf.aayus.me/4/A</p>
+</div>
